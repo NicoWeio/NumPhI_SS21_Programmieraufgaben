70d0967cc8fa0fc1691e01ccb61b9d7bc7f97b64
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2021-04-20 22:58:50 +0200

    Blatt 1 – Abgabe

--- Blatt 1/NumPhISS21_Programmierblatt1.m
+++ Blatt 1/NumPhISS21_Programmierblatt1.m
@@ -1,4 +1,4 @@
-%%  Numerische Mathematik fuer Physik und Ingenieurwissenschaften SS2021 
+%%  Numerische Mathematik fuer Physik und Ingenieurwissenschaften SS2021
 %
 %   Prof. Dr. J. Stoeckler
 %   M.Sc. M. Weimann
@@ -6,39 +6,49 @@
 %
 %   Programmierblatt 1
 %   Abgabe bis zum 21.04.2021
-%  
-%   Student*in 1: Vorname, Nachname, Matrikelnummer
-%   Student*in 2: Vorname, Nachname, Matrikelnummer
 %
-%   Programmversion: z.B. Matlab R2021a oder Octave 6.2.0
-%% 
+%   Student*in 1: Elias, Giannopoulos, 220848
+%   Student*in 2: Nicolai, Weitkemper, 220837
+%
+%   Programmversion: Octave 6.2.0
+%%
 
-close all; % close plots 
+close all; % close plots
 clear all; % delets all data in the Workspace
 
 %% Definition der Funktionen
-f = %... Definition als function handle 
+f = @(x) 10./(1+x.^2);
 
 % Polynom vierten Grades
-n = %... Grad
-x4 = %... Knoten mittels linspace
-y4 = %... Daten
-p4 = %... Interpolationspolynom mittels polyfit	
+n = 4; % Grad
+x4 = linspace(-5, 5, n+1); % Knoten mittels linspace
+y4 = f(x4); % Daten
+p4 = polyfit(x4, y4, n) % Interpolationspolynom mittels polyfit
 
 % Polynom zehnten Grades
-n = %... Grad
-x10 = %...
-y10 = %...
-p10 = %...	
+n = 10; % Grad
+x10 = linspace(-5, 5, n+1);
+y10 = f(x10);
+p10 = polyfit(x10, y10, n)
 
 %% Auswertung
 xx = linspace(-5,5,101); % diskrete Definitionsmenge
-yyf = %... Auswertung von f an den Stellen xx
-yy4 = %... Auswertung von p4 an den Stellen xx mittels polyval	
-yy10 = %...
-r4 = %... Interpolationsfehler zu p4
-r10 = %...
+yyf = f(xx); % Auswertung von f an den Stellen xx
+yy4 = polyval(p4, xx); % Auswertung von p4 an den Stellen xx mittels polyval
+yy10 = polyval(p10, xx);
+r4 = abs(yyf-yy4); % Interpolationsfehler zu p4
+r10 = abs(yyf-yy10);
 
 %% Ausgabe
-%...
-
+subplot(2,1,1)
+hold on
+plot(xx, yyf) % plotte die Ausgangs-Funktion
+plot(xx, yy4) % plotte Interpolation-Polynom 4. Grades
+plot(xx, yy10) % plotte Interpolation-Polynom 10. Grades
+hold off
+subplot(2,1,2)
+hold on
+plot(xx, r4) % plotte Interpolationsfehler zu p4
+plot(xx, r10) % plotte Interpolationsfehler zu p10
+hold off
+pause
